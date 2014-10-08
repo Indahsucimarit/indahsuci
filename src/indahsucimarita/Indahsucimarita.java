c49ba6c49e08c23b538f1d05f0bd3a7d8b928caa
MYP <sword.arch@yahoo.co.id>
2014-10-08 14:48:33 +0700

    commit

---
+++ src/indahsucimarita/Indahsucimarita.java
@@ -0,0 +1,18 @@
+
+package indahsucimarita;
+
+public class Indahsucimarita {
+
+    public static void main(String[] args) {
+      //memasukan nama ke dalam variabel
+        String nama="Indah Suci Marita";
+         System.out.println("Nama:"+nama);
+        String npm="13311033";
+         System.out.println("Npm:"+npm);
+         String kelas="SI 13E";
+         System.out.println("Kelas:"+kelas);
+        String kampus="Teknokrat";
+         System.out.println("Kampus:"+kampus);
+    }
+    
+}
